4b64bc3b4fa3608809bdee7da1f73d8ba1a4abf3
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-01 02:55:10 -0600

    added title, description, and table of contents into readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -30,7 +30,27 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+
 
 `;
 }
