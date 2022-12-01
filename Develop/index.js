05e5e5965e79de2c1a426aea6a59ac5b5f0138de
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-01 02:18:41 -0600

    adding questions to question array

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,16 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        question: "What is the project title?"
+    },
+    {
+        name: "description",
+        question: "What is the project description?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
