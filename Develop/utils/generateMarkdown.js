1a4d1f0af78e0625e65a2ed7bc11b05e3c203ca4
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-01 03:10:17 -0600

    removed starter code comments

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license !== "No license"){
     //return license badge here
@@ -8,8 +6,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license !== "No license"){
     //return license link
@@ -18,8 +14,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license !== "No license"){
     //return license section
@@ -28,7 +22,6 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
